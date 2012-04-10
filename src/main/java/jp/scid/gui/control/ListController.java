bdb2f2fdfc79c7e9872ef56901ace56b61832c2e
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2012-04-10 12:16:46 +0900

    リストコントローラの作成

---
+++ src/main/java/jp/scid/gui/control/ListController.java
@@ -0,0 +1,60 @@
+package jp.scid.gui.control;
+
+import javax.swing.JTable;
+
+import ca.odell.glazedlists.BasicEventList;
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.gui.TableFormat;
+import ca.odell.glazedlists.swing.EventSelectionModel;
+import ca.odell.glazedlists.swing.EventTableModel;
+
+public class ListController<E> {
+	final EventList<E> source;
+	
+	final EventSelectionModel<E> selectionModel;
+	
+	public ListController(EventList<E> list) {
+		this.source = list;
+		
+		selectionModel = new EventSelectionModel<E>(list);
+	}
+	
+	public ListController() {
+		this(new BasicEventList<E>());
+	}
+	
+	public EventList<E> getSource() {
+		return source;
+	}
+	
+	public EventSelectionModel<E> getSelectionModel() {
+		return selectionModel;
+	}
+	
+	public void bindTable(JTable table, TableFormat<E> format) {
+		EventTableModel<E> tableModel = createTableModel(format);
+		
+		table.setModel(tableModel);
+	}
+
+	protected EventTableModel<E> createTableModel(TableFormat<E> format) {
+		EventTableModel<E> tableModel = new EventTableModel<E>(getSource(), format);
+		return tableModel;
+	}
+	
+	public void add() {
+		int index = selectionModel.getMaxSelectionIndex() + 1;
+		
+		add(index);
+	}
+	
+	public void add(int index) {
+		E newElement = createElement();
+		
+		source.add(index, newElement);
+	}
+	
+	protected E createElement() {
+		throw new UnsupportedOperationException("must implement to create element");
+	}
+}
